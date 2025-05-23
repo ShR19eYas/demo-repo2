5c6e7e9e8c3cefd15621c0dcc8cabd721952df2a
ShR19eYas <shreyasml.1925@gmail.com>
2025-05-23 15:37:35 +0530

    Added a sum file

---
+++ sum.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+
+int main () {
+    int a,b;
+    printf("Enter two number : ");
+    scanf("%d,%d",&a,&b);
+
+    int sum = a + b;
+    printf("Sum : %d",sum);
+
+    return 0;
+}
